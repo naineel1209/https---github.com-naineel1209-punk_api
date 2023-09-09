3d753e4b379ff317da22333d062b905e5122b643
naineel1209 <soyantarnaineel@gmail.com>
2023-09-10 00:49:13 +0530

    done and dusted

--- src/components/BeerDataList.jsx
+++ src/components/BeerDataList.jsx
@@ -9,6 +9,14 @@ const BeerDataList = ({ beerData }) => {
     </>;
   }
 
+  if (beerData.length === 0) {
+    return (
+      <div className='flex flex-col items-center justify-center relative'>
+        <h1 className='text-3xl font-bold my-[2rem]'>No Results Found</h1>
+      </div>
+    )
+  }
+
   return (
     <>
       <div className=' border-black grid grid-cols-3 gap-5 p-5'>
